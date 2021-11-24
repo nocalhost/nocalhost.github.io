66b96a1e2107afb0e562a54fdb5f1a6721a4d9e1
yuanganping <562720180@qq.com>
2021-11-24 14:30:00 +0800

    fix: dark mode style (#143)

--- src/components/VideoModal.js
+++ src/components/VideoModal.js
@@ -1,22 +1,48 @@
-import React from 'react'
-// import {  createPortal } from 'react-dom'
-import styles from '../pages/index.module.css'
-// const body = document.getElementsByTagName('body')
+import React from "react";
+import styles from "../pages/index.module.scss";
 
-function VideoModal (props) {
-	const Div = (<div className={styles.modal} onClick={() => props.setShowVideoModa(false)}>
-		<div className={styles.modalMask}>
-			<div className={styles.contentVideoContainer}>
-				<video controls autoPlay width="100%" poster="img/poster-en.jpg" style={{ objectFit: 'cover', borderRadius: '10px' }} >
-					<source src="https://nocalhost-1301578102.cos.ap-nanjing.myqcloud.com/nh-overview-eng-20210714.mp4" type="video/mp4" />
-					<track src="video/nh-overview-eng.vtt" kind="subtitles" label="en text track" srcLang="en" default />
-					<track src="video/nh-overview.chn.vtt" kind="subtitles" label="中文字幕" srcLang="zh" />
-					<p>Your browser doesn&apos;t support HTML5 video. Here is a <a href="https://nocalhost-1301578102.cos.ap-nanjing.myqcloud.com/nh-overview-eng-20210714.mp4">link to the video</a> instead.</p>
-				</video>
-			</div>
-		</div>
-	</div>)
-	return Div
+function VideoModal(props) {
+  const Div = (
+    <div className={styles.modal} onClick={() => props.setShowVideoModa(false)}>
+      <div className={styles.modalMask}>
+        <div className={styles.contentVideoContainer}>
+          <video
+            controls
+            autoPlay
+            width="100%"
+            poster="img/poster-en.jpg"
+            style={{ objectFit: "cover", borderRadius: "10px" }}
+          >
+            <source
+              src="https://nocalhost-1301578102.cos.ap-nanjing.myqcloud.com/nh-overview-eng-20210714.mp4"
+              type="video/mp4"
+            />
+            <track
+              src="video/nh-overview-eng.vtt"
+              kind="subtitles"
+              label="en text track"
+              srcLang="en"
+              default
+            />
+            <track
+              src="video/nh-overview.chn.vtt"
+              kind="subtitles"
+              label="中文字幕"
+              srcLang="zh"
+            />
+            <p>
+              Your browser doesn&apos;t support HTML5 video. Here is a{" "}
+              <a href="https://nocalhost-1301578102.cos.ap-nanjing.myqcloud.com/nh-overview-eng-20210714.mp4">
+                link to the video
+              </a>{" "}
+              instead.
+            </p>
+          </video>
+        </div>
+      </div>
+    </div>
+  );
+  return Div;
 }
 
-export default VideoModal
+export default VideoModal;
